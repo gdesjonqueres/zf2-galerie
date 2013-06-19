709215d2f6669d0081313424ee2d1cc9e34434e7
guillaume <g.desjonqueres@gmail.com>
2013-06-19 22:54:37 +0200

    autoloader

---
+++ module/Galerie/Module.php
@@ -0,0 +1,25 @@
+<?php
+namespace Galerie;
+
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+
+class Module implements
+    AutoloaderProviderInterface,
+    ConfigProviderInterface
+{
+    public function getAutoloaderConfig()
+    {
+//         return array(
+//             'Zend\Loader\StandardAutoloader' => array(
+//                 'namespaces' => array(
+//                     __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__),
+//             ),
+//         );
+        return array(
+            'Zend\Loader\ClassMapAutoloader' => array(
+                __DIR__ . '/autoload_classmap.php',
+            ),
+        );
+    }
+}
